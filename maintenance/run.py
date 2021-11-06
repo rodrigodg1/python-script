35ef5cc95eb47ad8e146a4a2790c977ce4b12a09
Rodrigo <rodrigopba1@gmail.com>
2021-11-05 23:33:31 -0300

    improve code

--- maintenance/run.py
+++ maintenance/run.py
@@ -29,23 +29,36 @@ def show_ip_addr():
     print(bcolors.WARNING + "\nIp Address:" + bcolors.ENDC)
     os.system('hostname -I')
 
-def install_dev_packages(git=True,cplusplus=True,c=True,make=True,python3=True):
+def install_dev_packages():
+
+    print("Updating repository")
+    time.sleep(3)
     os.system("sudo apt-get update")
-    if(git):
-        print("\nInstalling Git ...")
-        os.system("sudo apt-get install git -y")
-    if(cplusplus):
-        print("\nInstalling C++ ...")
-        os.system("sudo apt-get install c++ -y")
-    if(c):
-        print("\nInstalling C ...")
-        os.system("sudo apt-get install gcc -y")
-    if(make):
-        print("\nInstalling make ...")
-        os.system("sudo apt-get install make -y")
-    if(python3):
-        print("\nInstalling python 3 ...")
-        os.system("sudo apt-get install python3")
+
+
+    print("\nInstalling Git ...")
+    time.sleep(3)
+    os.system("sudo apt-get install git -y")
+
+    print("\nInstalling C++ ...")
+    time.sleep(3)
+    os.system("sudo apt-get install c++ -y")
+
+    print("\nInstalling C ...")
+    time.sleep(3)
+    os.system("sudo apt-get install gcc -y")
+
+    print("\nInstalling make ...")
+    time.sleep(3)
+    os.system("sudo apt-get install make -y")
+
+    print("\nInstalling python 3 ...")
+    time.sleep(3)
+    os.system("sudo apt-get install python3")
+
+    print("\nInstalling pip3 ...")
+    time.sleep(3)
+    os.system("sudo apt install python3-pip")
 
 while (True):
     op = input("""
@@ -70,10 +83,10 @@ while (True):
         if(install == "y"):
             install_dev_packages()
     elif (op == "5"):
-        install= input("Will be installed:  git, C++, GCC, make, python3. Continue ? y/n: ")
+        install= input("Will be installed:  git, C++, GCC, make, python3, pip3. Continue ? y/n: ")
         if(install == "y"):
             install_dev_packages()
     elif (op == "6"):
-        os.system("chmod +x maintenance/shutdown-auto.sh")
-        os.system("maintenance/shutdown-auto.sh")
+        os.system("chmod +x shutdown-auto.sh")
+        os.system("./shutdown-auto.sh")
         #subprocess.call(["sh", "./shutdown-auto.sh"])
